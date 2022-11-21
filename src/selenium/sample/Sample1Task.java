7024daceefd438a968bdcf6f9a542b85fbfe2c2a
Julija Hmelnicka <julija.hmelnicka@gmail.com>
2022-11-21 12:53:08 +0200

    Completed Sample1Task and Sample2Task

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -2,18 +2,32 @@ package selenium.sample;
 
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import selenium.ChangeToFileExtension;
+
+import java.io.File;
 
 public class Sample1Task {
+    static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
     @Test
     public void goToHomepage() throws Exception {
 //        TODO:
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new ChangeToFileExtension().extension());
+
 //         define driver
+        WebDriver driver = new ChromeDriver();
 //         go to https://kristinek.github.io/site/index2.html
+        driver.get("https://kristinek.github.io/site/index2.html");
+        System.out.println(driver.findElements(By.id("h1")));
 //         get title of page
+        System.out.println(driver.getTitle());
 //         get URL of current page
+        System.out.println(driver.getCurrentUrl());
 //         close browser
+        driver.quit();
+
     }
 }
