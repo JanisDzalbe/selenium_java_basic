b26fdaaa42884dd91188b3b1162a09ad2ba7764a
Julija Hmelnicka <julija.hmelnicka@gmail.com>
2022-11-23 09:39:58 +0200

    Completed Sample8Task

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -38,5 +38,11 @@ public class Sample8Task {
 //        check the background of top 2 sections
 //        rgba(255, 221, 221, 1);
 //        check h1 element font-size 64px
+        WebElement leftTop = driver.findElement(By.className("w3-pale-red"));
+        WebElement rightTop = driver.findElement(By.className("w3-pale-yellow"));
+        WebElement h1 = driver.findElement(By.cssSelector("h1"));
+        assertEquals("rgba(255, 221, 221, 1)",leftTop.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 204, 1)",rightTop.getCssValue("background-color"));
+        assertEquals("64px",h1.getCssValue("font-size"));
     }
 }
